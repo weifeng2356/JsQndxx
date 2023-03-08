8e38611508ca84828cee5eb20b185ff0a90e2cc0
weifeng2356 <weifeng2356@qq.com>
2023-03-08 15:07:39 +0800

    Update main_actions.py

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 通过 Github Actions 自动完成 **江苏省** 青年大学习
 
-
+## 2023-03-08 更新 已适配新版大学习
 
 ## 使用方法
 
@@ -12,19 +12,22 @@
 
 ### 获取 Cookie
 
-按照 [青年大学习抓包教程](https://yuzai.xyz/archives/c59a0c1a.html) 获取自己的 `laravel_session`
+[手机抓包教程](https://blog.csdn.net/nikeylee/article/details/125363582)
+
+按照 [青年大学习抓包教程](https://yuzaii.github.io/archives/c59a0c1a.html)（[备份](https://web.archive.org/web/20230306140515/https://yuzaii.github.io/archives/c59a0c1a.html)） 获取自己的 `laravel_session`
 
 ### 添加 Cookie 至 Secrets
 
-回到项目页面，依次点击 Settings --> Secrets --> Actions --> New repository secret
+回到项目页面，依次点击 Settings --> Secrets and variables --> Actions --> New repository secret
 
 建立名为 `LARAVEL_SESSION` 的 secret，值为先前获取的 `laravel_session`，最后点击 Add secret
 
-### 启动Github Actions
+### 启动 Github Actions
 
-> Actions 默认为关闭状态，Fork 之后需要手动执行一次，若成功运行其才会激活。
+> Actions 默认为关闭状态，Fork 之后需要**手动执行**一次，若成功运行其才会激活。
+> 此外，你需要**每 60 天**手动执行一次，不然会被 GitHub 暂停。
 
-返回项目主页面，点击上方的 `Actions`，再点击左侧的`Auto JsQndxx`，再点击`Run workflow`
+返回项目主页面，点击上方的 `Actions`，再点击左侧的 `Auto JsQndxx`，再点击 `Run workflow`
 
 至此，部署完毕。
 
@@ -37,6 +40,6 @@
 
 
 ## 感谢
+https://github.com/BeiyanYunyi/AntiFacistIndoctrination
 
-https://github.com/yuzaii/JsQndxx_Python
-
+~~https://github.com/yuzaii/JsQndxx_Python~~
--- main_actions.py
+++ main_actions.py
@@ -1,57 +1,64 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import re
 import requests
-import urllib3
-from bs4 import BeautifulSoup
 import os
 
+# 参数为 Cookie 里的 laravel_session, 请自行抓包获取
 laravel_session = os.environ['LARAVEL_SESSION']
 
-def main(laravel_session):  # 参数为cookie里的laravel_session 自行抓包获取
-    s = requests.session()  # 创建会话
-    loginurl = "https://service.jiangsugqt.org/youth/lesson"  # 江苏省青年大学习接口
+
+def get_latest_lesson(s, laravel_session):
+    url = "https://service.jiangsugqt.org/api/cjdList"  # 江苏省青年大学习成绩单接口
     # 参数
     params = {
-        "s": "/youth/lesson",
-        "form": "inglemessage",
-        "isappinstalled": "0"
+        "page": "1",
+        "limit": "20"
     }
     # 构造请求头
     headers = {
-        'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN",
-        'Cookie': "laravel_session=" + laravel_session  # 抓包获取
+        'User-Agent': "Mozilla/5.0 (Linux; Android 13; 22127RK46C Build/TKQ1.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 XWEB/5015 MMWEBSDK/20230202 MMWEBID/3840 MicroMessenger/8.0.33.2320(0x2800213D) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        'Cookie': "laravel_session=" + laravel_session
     }
-    urllib3.disable_warnings()  # 不然会有warning
-    login = s.get(url=loginurl, headers=headers, params=params, verify=False)  # 登录
+    res = s.post(url=url, headers=headers, params=params)  # 发送请求
+    res = res.json()  # 返回结果转json
+    if res["status"] == 1 and res["message"] == "操作成功":
+        return res["data"][0]
+    else:
+        return None
 
-    login_soup = BeautifulSoup(login.text, 'html.parser')  # 解析信息确认页面
-    userinfo = login_soup.select(".confirm-user-info p")  # 找到用户信息div 课程姓名编号单位
 
-    dict = {}  # 构建用户信息字典
-
-    for i in userinfo:
-        info_soup = BeautifulSoup(str(i), 'html.parser')  # 分布解析课程姓名编号单位信息
-        item = info_soup.get_text()  # 用户信息
-        dict[item[:4]] = item[5:]
-    token = re.findall(r'var token ?= ?"(.*?)"', login.text)  # 获取js里的token
-    lesson_id = re.findall(r"'lesson_id':(.*)", login.text)  # 获取js里的token
-    
-    dict['token'] = token[0]
-    dict['lesson_id'] = lesson_id[0]
-
-    confirmurl = "https://service.jiangsugqt.org/youth/lesson/confirm"
+def learn_lesson(s, laravel_session, lesson_id):
+    url = "https://service.jiangsugqt.org/api/doLesson"  # 江苏省青年大学习接口
+    # 参数
     params = {
-        "_token": token[0],
-        "lesson_id": lesson_id[0]
+        "lesson_id": lesson_id
     }
-    res = s.post(url=confirmurl, params=params)
+    # 构造请求头
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Linux; Android 13; 22127RK46C Build/TKQ1.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 XWEB/5015 MMWEBSDK/20230202 MMWEBID/3840 MicroMessenger/8.0.33.2320(0x2800213D) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        'Cookie': "laravel_session=" + laravel_session
+    }
+    res = s.post(url=url, headers=headers, params=params)  # 发送请求
     res = res.json()  # 返回结果转json
     print("%s: %s" % (lesson_id[0], res))
     if res["status"] == 1 and res["message"] == "操作成功":
         pass
     else:
-        print("error")
+        print("学习失败")
+
+
+def main(laravel_session):
+    s = requests.session()  # 创建会话
+    latest_lesson = get_latest_lesson(s, laravel_session)
+    if laravel_session == None:
+        print("无法查询成绩单，请检查 Cookie 是否正确")
+        return
+    if latest_lesson["has_learn"] == '1':
+        print("本周的 %s 已经学过了" % latest_lesson["title"])
+        return
+
+    learn_lesson(s, laravel_session, latest_lesson["id"])
+
 
 if __name__ == '__main__':
     main(laravel_session)
