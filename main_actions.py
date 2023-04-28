1cd3b94360ba4c22d0dd13a451d9d618aa18df29
weifeng2356 <weifeng2356@qq.com>
2023-04-28 19:44:13 +0800

    Update User-Agent

--- main_actions.py
+++ main_actions.py
@@ -16,7 +16,7 @@ def get_latest_lesson(s, laravel_session):
     }
     # 构造请求头
     headers = {
-        'User-Agent': "Mozilla/5.0 (Linux; Android 13; 22127RK46C Build/TKQ1.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 XWEB/5015 MMWEBSDK/20230202 MMWEBID/3840 MicroMessenger/8.0.33.2320(0x2800213D) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN",
         'Cookie': "laravel_session=" + laravel_session
     }
     res = s.post(url=url, headers=headers, params=params)  # 发送请求
@@ -35,7 +35,7 @@ def learn_lesson(s, laravel_session, lesson_id):
     }
     # 构造请求头
     headers = {
-        'User-Agent': "Mozilla/5.0 (Linux; Android 13; 22127RK46C Build/TKQ1.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 XWEB/5015 MMWEBSDK/20230202 MMWEBID/3840 MicroMessenger/8.0.33.2320(0x2800213D) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN",
         'Cookie': "laravel_session=" + laravel_session
     }
     res = s.post(url=url, headers=headers, params=params)  # 发送请求
@@ -50,7 +50,7 @@ def learn_lesson(s, laravel_session, lesson_id):
 def main(laravel_session):
     s = requests.session()  # 创建会话
     latest_lesson = get_latest_lesson(s, laravel_session)
-    if laravel_session == None:
+    if latest_lesson == None:
         print("无法查询成绩单，请检查 Cookie 是否正确")
         return
     if latest_lesson["has_learn"] == '1':
